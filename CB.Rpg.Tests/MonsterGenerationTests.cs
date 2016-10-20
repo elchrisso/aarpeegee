96e82a07aba7b53222e9d8a53c7418263345e5bf
CBrown2 <CBrown2@CBROWN2-E7440>
2016-10-20 12:38:30 -0600

    saving changes for first commit from vs2015

---
+++ CB.Rpg.Tests/MonsterGenerationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CB.Rpg.Tests
+{
+    class MonsterGenerationTests
+    {
+        [Fact]
+        public void GenerateAMonster()
+        {
+            Monster newMonster = new Monster();
+        }
+    }
+}
---
+++ CB.Rpg/Monster.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CB.Rpg
+{
+    public class Monster
+    {
+        //public enum Species
+        //{
+            
+        //}
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public int Power { get; set; }
+        public int Speed { get; set; }
+        public int Life { get; set; }
+
+        public Monster(string name, int size, int power, int speed, int life)
+        {
+            Name = name;
+            Size = size;
+            Power = power;
+            Speed = speed;
+            Life = life;
+        }
+    }
+}
